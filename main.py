b5e26aee630b3d0b7f976344337b82b73b8aaea6
finan <finanmeharenakidane@gmail.com>
2022-08-26 13:53:46 +0400

    created the basic front end component / taken from bootstrap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 
 app=Flask(__name__)
@@ -6,8 +6,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'home'
-
+    return render_template('index.html')
 
 
 if __name__=='__main__':
